bf54e87a4bedc327beb9d38955ec3c9675edc28a
vladfidem <vladfidem@gmail.com>
2024-10-29 04:37:25 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export const App = () => {
+  return (
+    <div className="app-container">
+      <h1>React Query</h1>
+      <img className="app-logo" src="../public/react-query.svg" alt="logo"/>
+    </div>
+  )
+}
